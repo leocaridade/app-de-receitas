48fb1e8bf653cd013a5ed8c616ddaf57d5427b6d
Olivia Couto <olivia.acouto@gmail.com>
2023-05-05 17:29:16 -0300

    Feat Criando Rotas, Páginas e testes

---
+++ src/components/Routes.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from '../pages/Login';
+import Meals from '../pages/Meals';
+import Drinks from '../pages/Drinks';
+import Profile from '../pages/Profile';
+import Done from '../pages/Done';
+import Favorite from '../pages/Favorite';
+
+export default function Routes() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/meals" component={ Meals } />
+        {/* <Route exact path="/meals/:id-da-receita" component={ #Meals } /> */}
+        {/* <Route exact path="/meals/:id-da-receita/in-progress" component={ #Meals } /> */}
+        <Route exact path="/drinks" component={ Drinks } />
+        {/* <Route exact path="/drinks/:id-da-receita" component={ #Drinks } />
+        <Route exact path="/drinks/:id-da-receita/in-progress" component={ #Drinks } /> */}
+        <Route exact path="/profile" component={ Profile } />
+        <Route exact path="/done-recipes" component={ Done } />
+        <Route exact path="/favorite-recipes" component={ Favorite } />
+      </Switch>
+    </div>
+  );
+}
