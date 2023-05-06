f16f456e756cf707376e3187cc0b0fe7e2b82356
TMendes-lucca <luccatomazetto@gmail.com>
2023-05-05 21:45:37 -0300

    feat: Requisitos 57, 58 e 59 ok no cypress

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Profile() {
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    const userEmail = user ? user.email : '';
+    document.querySelector('[data-testid="profile-email"]').innerHTML = userEmail;
+  }, []);
+
   return (
     <>
       <Header title="Profile" searchBtn={ false } />
-      <div>Profile</div>
+      <div>
+        <p>Hello, user, this is your Profile</p>
+        <p data-testid="profile-email" />
+        <button data-testid="profile-done-btn">Done Recipes</button>
+        <button data-testid="profile-favorite-btn">Favorite Recipes</button>
+        <button data-testid="profile-logout-btn">Logout</button>
+      </div>
       <Footer />
     </>
   );
