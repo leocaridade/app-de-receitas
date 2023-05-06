ffa38dbc0b24d992306e64f2eb48966b8fce2dcc
TMendes-lucca <luccatomazetto@gmail.com>
2023-05-06 00:11:19 -0300

    fix: lint fix

--- src/tests/03.Profile.test.js
+++ src/tests/03.Profile.test.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import userEvent from '@testing-library/user-event';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import renderWithRouter from './helpers/renderWithRouter';
-import Login from '../pages/Login';
 import Profile from '../pages/Profile';
-
-const validEmail = 'email@teste.com';
-const validPassword = '1234567';
 
 describe('Teste a página de profile', () => {
   test('Testa se os botões de perfil, receitas feitas, receitas favoritas e sair estão na tela.', () => {
