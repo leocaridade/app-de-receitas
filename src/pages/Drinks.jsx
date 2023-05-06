b132b39a2f1a60032be4b30ef061a0e072a5a54b
TMendes-lucca <luccatomazetto@gmail.com>
2023-05-05 21:27:45 -0300

    feat: requisito 17 sem cobertura de testes

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Drinks() {
   return (
     <>
       <Header title="Drinks" searchBtn />
       <div>Drinks</div>
+      <Footer />
     </>
   );
 }
