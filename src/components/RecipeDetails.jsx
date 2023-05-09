97d9632a95c404fd3d90a731248a329242719b63
TMendes-lucca <luccatomazetto@gmail.com>
2023-05-08 23:31:05 -0300

    feat: requisito 25 cypress ok

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -1,24 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import { fetchFoodByIdAPI } from '../services/mealsAPI';
 import { fetchDrinkByIdAPI } from '../services/drinksAPI';
 
 function RecipeDetails({ recipeType }) {
+  const [recipeDetails, setRecipeDetails] = useState([]);
+  const [ingredientDetails, setIngredientDetails] = useState([]);
   const history = useHistory();
-  console.log(history);
 
   useEffect(() => {
     const recipeID = history.location.pathname.split('/')[2];
     const handleFetchDetails = async () => {
       try {
-        let recipeDetails;
+        let recipeDetail;
+        let recipeIngredient;
         if (recipeType === 'meals') {
-          recipeDetails = await fetchFoodByIdAPI(recipeID);
+          recipeDetail = await fetchFoodByIdAPI(recipeID);
+          recipeIngredient = Object
+            .entries(recipeDetail[0])
+            .filter(([key, value]) => key.startsWith('strIngredient') && value);
         }
         if (recipeType === 'drinks') {
-          recipeDetails = await fetchDrinkByIdAPI(recipeID);
+          recipeDetail = await fetchDrinkByIdAPI(recipeID);
+          recipeIngredient = Object
+            .entries(recipeDetail[0])
+            .filter(([key, value]) => key.startsWith('strIngredient') && value);
         }
+        setRecipeDetails(recipeDetail);
+        setIngredientDetails(recipeIngredient);
+        console.log(recipeDetail);
       } catch (error) {
         console.log(error);
       }
@@ -29,6 +40,39 @@ function RecipeDetails({ recipeType }) {
   return (
     <div>
       <p>{`Hello World! Your recipe type is: ${recipeType}`}</p>
+      {recipeDetails.map((recipe, index) => (
+        <div key={ index }>
+          <p data-testid="recipe-title">{recipe.strDrink || recipe.strMeal}</p>
+          {recipe.strAlcoholic && (
+            <p data-testid="recipe-category">{recipe.strAlcoholic}</p>
+          )}
+          <p data-testid="recipe-category">{recipe.strCategory}</p>
+          <div>
+            <p>Ingredients:</p>
+            {ingredientDetails.map((ingredient, i) => (
+              <p key={ i } data-testid={ `${i}-ingredient-name-and-measure` }>
+                {`${ingredient[1]} - ${recipe[`strMeasure${i + 1}`]}`}
+              </p>
+            ))}
+          </div>
+          <p> Instructions: </p>
+          <p data-testid="instructions">{recipe.strInstructions}</p>
+          {recipeType === 'meals' && (
+            <iframe
+              data-testid="video"
+              title="video"
+              width="320"
+              height="240"
+              src={ recipe.strYoutube.replace('watch?v=', 'embed/') }
+            />
+          )}
+          <img
+            src={ recipe.strDrinkThumb || recipe.strMealThumb }
+            alt="Product"
+            data-testid="recipe-photo"
+          />
+        </div>
+      ))}
     </div>
   );
 }
