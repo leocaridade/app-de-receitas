5c3471a72a2147fc481ea88b8263dea3d590250e
leocaridade <leocaridade@hotmail.com>
2023-05-10 09:29:47 -0400

    fix: Arruma a logica de salvar os checkbox no localStorage, estilo das labels e salvar a URL no clipboard

--- src/pages/RecipeInProgress.jsx
+++ src/pages/RecipeInProgress.jsx
@@ -3,14 +3,13 @@ import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import copy from 'clipboard-copy';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
 import whiteHeartIcon from '../images/whiteHeartIcon.svg';
-import { fetchDrinkByIdAPI, fetchDrinksAPI } from '../services/drinksAPI';
-import { fetchFoodByIdAPI, fetchMealsAPI } from '../services/mealsAPI';
+import { fetchDrinkByIdAPI } from '../services/drinksAPI';
+import { fetchFoodByIdAPI } from '../services/mealsAPI';
 import { getLocalStorage, setLocalStorage } from '../services/localStorage';
 
 function RecipeInProgress() {
   const [recipeDetails, setRecipeDetails] = useState([]);
   const [ingredientDetails, setIngredientDetails] = useState([]);
-  const [recipeInProgress, setRecipeInProgress] = useState(false);
 
   const [isLinkCopied, setIsLinkCopied] = useState(false);
   const [favoriteIcon, setFavoriteIcon] = useState(false);
@@ -26,18 +25,15 @@ function RecipeInProgress() {
   useEffect(() => {
     const handleFetchDetails = async () => {
       try {
-        let baseRecipesAPI;
         let recipeDetail;
         let recipeIngredient;
         if (recipeType === 'meals') {
-          baseRecipesAPI = await fetchDrinksAPI();
           recipeDetail = await fetchFoodByIdAPI(recipeID);
           recipeIngredient = Object
             .entries(recipeDetail[0])
             .filter(([key, value]) => key.startsWith('strIngredient') && value);
         }
         if (recipeType === 'drinks') {
-          baseRecipesAPI = await fetchMealsAPI();
           recipeDetail = await fetchDrinkByIdAPI(recipeID);
           recipeIngredient = Object
             .entries(recipeDetail[0])
@@ -45,13 +41,12 @@ function RecipeInProgress() {
         }
         setRecipeDetails(recipeDetail);
         setIngredientDetails(recipeIngredient);
-        console.log(baseRecipesAPI);
       } catch (error) {
         console.log(error);
       }
     };
     handleFetchDetails();
-  }, [history.location.pathname]);
+  }, [recipeID, recipeType]);
 
   useEffect(() => {
     if (getLocalStorage('doneRecipes') !== null) {
@@ -62,10 +57,9 @@ function RecipeInProgress() {
       }
     }
     if (getLocalStorage('inProgressRecipes') !== null) {
-      const recipesInProgress = getLocalStorage('inProgressRecipes');
-      if (recipesInProgress[recipeType][recipeID]) {
-        setRecipeInProgress(true);
-        setCheckboxValues(recipesInProgress[recipeType][recipeID]);
+      const recipesInProgress = getLocalStorage('inProgressRecipes') || {};
+      if (recipesInProgress[recipeType] && recipesInProgress[recipeType][recipeID]) {
+        setCheckboxValues(recipesInProgress);
       }
     }
     if (getLocalStorage('favoriteRecipes') !== null) {
@@ -76,11 +70,12 @@ function RecipeInProgress() {
         setFavoriteIcon(true);
       }
     }
-  }, [recipeID]);
+  }, [recipeType, recipeID]);
 
   const handleShareButton = () => {
     const URL = window.location.href;
-    copy(URL);
+    const modifiedURL = URL.replace('/in-progress', '');
+    copy(modifiedURL);
     setIsLinkCopied(true);
     setTimeout(() => setIsLinkCopied(false), LINK_COPIED_MESSAGE_TIME);
   };
@@ -118,20 +113,33 @@ function RecipeInProgress() {
   };
 
   const handleCheckboxChange = (event) => {
-    setCheckboxValues((prevState) => {
-      const updatedCheckboxes = {
-        ...prevState,
+    const { name, checked } = event.target;
+    setCheckboxValues((prevState) => ({
+      ...prevState,
+      [recipeType]: {
+        ...prevState[recipeType],
+        [recipeID]: {
+          ...(prevState[recipeType]?.[recipeID] || {}),
+          [name]: checked,
+        },
+      },
+    }));
+
+    try {
+      const inProgressRecipes = getLocalStorage('inProgressRecipes') || {};
+      setLocalStorage('inProgressRecipes', {
+        ...inProgressRecipes,
         [recipeType]: {
-          ...prevState[recipeType],
+          ...(inProgressRecipes[recipeType] || {}),
           [recipeID]: {
-            ...prevState[recipeType]?.[recipeID],
-            [event.target.name]: event.target.checked,
+            ...(inProgressRecipes[recipeType]?.[recipeID] || {}),
+            [name]: checked,
           },
         },
-      };
-      setLocalStorage('inProgressRecipes', updatedCheckboxes);
-      return updatedCheckboxes;
-    });
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -170,7 +178,7 @@ function RecipeInProgress() {
                 data-testid={ `${i}-ingredient-step` }
                 style={ {
                   textDecoration: (
-                    checkboxValues[`ingredientstep${i}`]
+                    checkboxValues[recipeType]?.[recipeID]?.[`ingredientstep${i}`]
                       ? 'line-through solid rgb(0, 0, 0)' : 'none'
                   ),
                 } }
@@ -179,7 +187,8 @@ function RecipeInProgress() {
                   type="checkbox"
                   id={ `ingredientstep${i}` }
                   name={ `ingredientstep${i}` }
-                  checked={ checkboxValues[`ingredientstep${i}`] }
+                  checked={ checkboxValues[recipeType]?.
+                    [recipeID]?.[`ingredientstep${i}`] || false }
                   onChange={ handleCheckboxChange }
                 />
                 {`${ingredient[1]} - ${recipe[`strMeasure${i + 1}`]}`}
@@ -204,13 +213,6 @@ function RecipeInProgress() {
           />
         </div>
       ))}
-      <button
-        id="start-recipe-btn"
-        data-testid="start-recipe-btn"
-        className="fixed bottom-0"
-      >
-        Start Recipe
-      </button>
       <button
         data-testid="finish-recipe-btn"
       >
